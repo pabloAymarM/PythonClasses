4682e8e9db87154fdd604e74d53cd124c33e2703
Pablo Aymar <pablomdocarmo@gmail.com>
2024-09-18 09:38:38 -0300

    Aula fuction and dictionary

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+pessoa = ['Maria', '48', 'Rua 10']
+print(pessoa)
+
+#starting
+dadosPessoais = {
+    'nome:':'João',
+    'Idade':23,
+    'Endereço':'Avenida 4'
+}
+
+print(dadosPessoais)
+
+#print without {}
+print(dadosPessoais.keys())
+
+#print separate without {}
+print(dadosPessoais.values())
+
+#print separate
+print(dadosPessoais.items())
+
+#dictionary with details
+for chave, valor in dadosPessoais.items():
+    print(f'{chave} : {valor}')
+    
+#add in dictionary
+dadosPessoais['Peso'] = 68
+print(dadosPessoais)
+
+dadosPessoais.update({'Profissão':'Engenheiro'})
+print(dadosPessoais)
+
+#delete in dictionary
+dadosPessoais.pop('Endereço')
+print(dadosPessoais)
